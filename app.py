43e3cdebbd323baf773bc8c08efa07388c54bd26
JXSnack <jx.snack@gmail.com>
2024-09-17 13:10:14 +0200

    load with I18N

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from i18n import I18NEngine
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    engine = I18NEngine("en_us")
+    engine.load()
+    return render_template("index.html", i18n=engine.get)
 
 
 if __name__ == '__main__':
