47185c0a466c615ec7efbedfcbcfc57efb77b28f
JXSnack <jx.snack@gmail.com>
2024-09-17 19:45:44 +0200

    password creation and verification

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import hashlib
+import secrets
 import time
 
 
@@ -10,3 +12,18 @@ def millis() -> int:
 
 def has_keys(keys: list, check: dict) -> bool:
     return all(key in check for key in keys)
+
+
+def create_password(original: str) -> list[str]:
+    salt = secrets.token_hex(16)
+    salted_password = salt + original
+    hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()
+
+    return [salt, hashed_password]
+
+
+def verify_password(salt: str, hashed_password: str, input_password: str) -> bool:
+    salted_input_password = salt + input_password
+    hashed_input_password = hashlib.sha256(salted_input_password.encode()).hexdigest()
+
+    return hashed_input_password == hashed_password
