714f0e2ec3d21980c12782ccfd9bee50a071669e
JXSnack <jx.snack@gmail.com>
2024-09-17 18:27:37 +0200

    has_keys method

--- helper.py
+++ helper.py
@@ -6,3 +6,7 @@ def millis() -> int:
     :return: Returns the current time in milliseconds
     """
     return round(time.time() * 1000)
+
+
+def has_keys(keys: list, check: dict) -> bool:
+    return all(key in check for key in keys)
