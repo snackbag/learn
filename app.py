50815ee8dd9af666907b380a2f03f4d149433faa
JXSnack <jx.snack@gmail.com>
2024-09-17 18:27:51 +0200

    secret key and base checking

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+
+import helper
 from cache import Globals
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dev"
 
 
 def i18n_get(lang: str = "en_us"):
@@ -20,6 +23,26 @@ def login():
 
 @app.route("/login/student", methods=["GET", "POST"])
 def login_student():
+    if request.method == "POST":
+        if not helper.has_keys(["classcode", "username", "password"], request.form):
+            return "Illegal request: Request form requires classcode, username and password"
+
+        classcode = request.form["classcode"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if len(classcode) != 8:
+            return "Classcode must be 8 characters long"
+
+        if len(username) < 2 or len(username) > 20:
+            return "Username must be between 2 and 20 characters long"
+
+        if len(password) < 4 or len(password) > 30:
+            return "Password must be between 4 and 30 characters long"
+
+        i18n = i18n_get()
+        flash(i18n("error.notfound.class"))
+
     return render_template("login/student.html", i18n=i18n_get())
 
 
