929bef500957753b5a3f7c22cda6908f81cfc461
JXSnack <jx.snack@gmail.com>
2024-09-17 16:39:01 +0200

    thingies

--- app.py
+++ app.py
@@ -18,5 +18,20 @@ def login():
     return render_template("login.html", i18n=i18n_get())
 
 
+@app.route("/login/student")
+def login_student():
+    return render_template("base.html", i18n=i18n_get())
+
+
+@app.route("/login/teacher")
+def login_teacher():
+    return render_template("base.html", i18n=i18n_get())
+
+
+@app.route("/login/personal")
+def login_personal():
+    return render_template("base.html", i18n=i18n_get())
+
+
 if __name__ == '__main__':
     app.run()
