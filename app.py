faf446afa2747ddad37a4e2c244e7bedd5aa1f88
JXSnack <jx.snack@gmail.com>
2024-09-17 13:48:28 +0200

    easier writing

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from cache import Globals
 app = Flask(__name__)
 
 
+def i18n_get(lang: str):
+    return Globals.i18n_cache.get_or_create(lang).get
+
+
 @app.route('/')
 def index():
-    return render_template("index.html", i18n=Globals.i18n_cache.get_or_create("en_us").get)
+    return render_template("index.html", i18n=i18n_get("de_de"))
 
 
 if __name__ == '__main__':
